97905c7055f2de86de5e84f6b307e9f2792d4fc8
OloioiOl <chansik4294@cau.ac.kr>
2026-08-26 11:24:39 +0900

    Create PersonManager

---
+++ src/workshop/person/control/PersonManager.java
@@ -0,0 +1,8 @@
+package workshop.person.control;
+from workshop.person.entity 
+
+public class PersonManager {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/workshop/person/entity/PersonEntity.java
@@ -0,0 +1,93 @@
+package workshop.person.entity;
+
+public class PersonEntity {
+	private String name;
+	private char gender;
+	private String ssn;
+	private String address;
+	private String phone;
+	
+	public PersonEntity() {
+		
+	}
+	
+	public PersonEntity(String name,String ssn,String address,String phone) {
+		this.name=name;
+		setSsn(ssn);
+		this.address=address;
+		this.phone=phone;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public char getGender() {
+		return gender;
+	}
+
+
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+
+
+	public String getSsn() {
+		return ssn;
+	}
+
+
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+		//030303-3543232
+		char genderNum = ssn.charAt(6);
+		if(genderNum == '1' || genderNum == '3') {
+			setGender('남');
+		}else {  //2,4
+			setGender('여');
+		}
+	}
+
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
